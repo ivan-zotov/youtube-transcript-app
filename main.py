b7b2863a9955a9f722cb31f6c6f275884e49d385
ivan-zotov <2ivanzotov@gmail.com>
2025-01-27 21:08:03 +0300

    Configure app to use environment port

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 from youtube_transcript_api import YouTubeTranscriptApi
 
@@ -11,7 +12,15 @@ def home():
 def get_transcript():
     try:
         url = request.form['url']
-        video_id = url.split('v=')[1].split('&')[0]
+        # Извлечение ID видео из URL
+        if 'v=' in url:
+            video_id = url.split('v=')[1].split('&')[0]
+        elif 'youtu.be/' in url:
+            video_id = url.split('youtu.be/')[1].split('?')[0]
+        else:
+            return jsonify({'text': None, 'error': 'Неверный формат URL.'})
+        
+        # Получение транскрипта
         transcript = YouTubeTranscriptApi.get_transcript(video_id, languages=['ru', 'en'])
         text = ' '.join([entry['text'] for entry in transcript])
         return jsonify({'text': text, 'error': None})
@@ -19,4 +28,5 @@ def get_transcript():
         return jsonify({'text': None, 'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
